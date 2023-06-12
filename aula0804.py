953670781e5b20e683245d28ffb50969eeefbeba
Leticia Pinheiro <127361649+Leticiapinheiro1@users.noreply.github.com>
2023-06-12 17:31:36 -0300

    Delete aula0804.py

--- aula0804.py
+++
@@ -1,8 +0,0 @@
-frase = input("Digite uma frase: ")
-frase = frase.lower()
-contador_vogais = 0
-for caractere in frase:
-    if caractere in 'aeiou':
-        contador_vogais += 1
-
-print("Quantidade de vogais na frase:", contador_vogais)
