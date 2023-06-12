bd09ccabdacc4160301bb455d499cf3181710475
Leticia Pinheiro <127361649+Leticiapinheiro1@users.noreply.github.com>
2023-06-12 17:30:53 -0300

    Delete aula0802.py

--- aula0802.py
+++
@@ -1,7 +0,0 @@
-data=input("Qual a sua data de aniversario? dd/mm/aaaa:")
-dia = data[:2]
-mes = data[3:5]
-ano = data[6:10]
-data_formato_novo = ano + mes + dia
-
-print("Data no formato AAAAMMDD:", data_formato_novo)
